4ccece32a2582381e162ec89cb28a58b21a478e9
Lauren <lauren.elisiario@proton.me>
2024-09-09 21:57:22 -0700

    Finished Side Navigation bar.

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
 // import Sidebar from '../styles/sidebar.css'; --Separate file import for sidebar styles.
 
 const Sidebar = ({ isOpen, handleDrawerToggle }) => {
+  const [openOfficials, setOpenOfficials] = useState(false);
+
+  const handleOfficialsClick = () => {
+    setOpenOfficials(!openOfficials);
+  };
   return (
     <Drawer
       variant="temporary"
@@ -16,6 +23,7 @@ const Sidebar = ({ isOpen, handleDrawerToggle }) => {
           backgroundColor: '#FDD686',
           color: '#252D30',
           width: '240px',
+          top: '64px',
           opacity: 0.7,
         },
       }}
@@ -27,9 +35,30 @@ const Sidebar = ({ isOpen, handleDrawerToggle }) => {
         <ListItem button>
           <ListItemText primary="Legislation" />
         </ListItem>
-        <ListItem button>
+
+        <ListItem button onClick={handleOfficialsClick}>
           <ListItemText primary="Elected Officials" />
+          {openOfficials ? <ExpandLess /> : <ExpandMore />}
         </ListItem>
+        <Collapse in={openOfficials} timeout="auto" unmountOnExit>
+        <list component="div" disablePadding>
+          <ListItem button>
+            <ListItemText primary="House of Representatives" />
+          </ListItem>
+          <ListItem button>
+          <ListItemText primary="Senators" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="President" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="Vice President" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="SCOTUS" />
+            </ListItem>
+        </list>
+        </Collapse>
         <ListItem button>
           <ListItemText primary="Educational Resources" />
         </ListItem>
