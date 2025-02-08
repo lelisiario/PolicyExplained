90bd9f07a7451e8d80f68aff5704b3f320d7c144
Lauren <lauren.elisiario@proton.me>
2025-02-08 14:32:33 -0800

    Added auth import for Firebase to AuthCOntext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase"; // Adjust the import to your firebase.js
+import { auth } from "../firebase";
 
 export const AuthContext = createContext();
 
