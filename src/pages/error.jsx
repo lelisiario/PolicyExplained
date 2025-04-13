930aa6678f7cdf766e430d6ee152e742e862cadb
Lauren <lauren.elisiario@proton.me>
2025-04-13 15:13:22 -0700

    changed error.jsx

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,16 +1,10 @@
-import { useRouteError } from 'react-router-dom';
-
-export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
-
+const ErrorPage = () => {
   return (
-    <div id="error-page">
+    <div>
       <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      <p>Something went wrong.</p>
     </div>
   );
-}
+};
+
+export default ErrorPage;
